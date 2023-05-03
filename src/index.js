f71f531c37f66d55fac33acae911d31c151e57a2
unknown <samjoshua.caballero@g.msuiit.edu.ph>
2023-05-03 17:54:20 +0800

    game-console-version

--- src/index.js
+++ src/index.js
@@ -44,17 +44,29 @@ const auth = getAuth(app)
 
 var container2 = document.querySelector('.container2')
 var container = document.querySelector('.container')
+const container4 = document.querySelector('.container4')
+const container5 = document.querySelector('.container5')
+var container3 = document.querySelector('.container3')
 var user_status = 0
 
 var user_status = new URLSearchParams(window.location.search).get('user_status')
 
 if (user_status == 1){
     container.style.display = 'none'
+    container4.style.display = 'none'
+    container5.style.display = 'none'
+    container3.style.display = 'none'
+    container6.style.display = 'none'
     container2.style.display = ""
 }else{
     console.log('no one is logged in')
     container2.style.display = 'none'
+    container4.style.display = 'none'
+    container5.style.display = 'none'
+    container6.style.display = 'none'
+    container3.style.display = 'none'
 }
+
 
 
 
@@ -185,6 +197,7 @@ twitterLogin.addEventListener('click', (e) => {
         // ...
         container.style.display = "none"
         container2.style.display = ""
+
     }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
@@ -218,6 +231,7 @@ facebookLogin.addEventListener('click', (e) => {
 
       container.style.display = "none"
       container2.style.display = ""
+
     })
     .catch((error) => {
       // Handle Errors here.
@@ -252,6 +266,7 @@ githubLogin.addEventListener('click', (e) => {
 
         container.style.display = "none"
         container2.style.display = ""
+
     }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
@@ -270,7 +285,7 @@ githubLogin.addEventListener('click', (e) => {
 // Database
 const db = getFirestore()
 // collection reference
-const colRef = collection(db, 'comments')
+const colRef = collection(db, 'game-consoles')
 // get collection data
 getDocs(colRef)
     .then((snapshot) => {
@@ -288,26 +303,110 @@ getDocs(colRef)
     })
 
 // CRUD
-const itemList = document.querySelector('.read')
+const itemList = document.querySelector('#read')
 const crudForm = document.querySelector('.crud')
-const createButton = document.querySelector('.button2')
+// createButton.addEventListener('click', (e) => {
+//     e.preventDefault()
+
+//     showCreate()
+
+//     addDoc(colRef, { comment: crudForm.comment.value })
+//         .then(() => {
+//             crudForm.reset()
+//             if (user_status == 1) {
+//                 location.href = location.href
+//             }else{
+//                 location.replace(location.href + "?user_status=1")
+//             }
+
+//         })
+// })
+
+const createForm = document.querySelector('.create_cont')
+const updateForm = document.querySelector('.update_cont')
+const createButton = document.querySelector('#create_button')
+const cancelButton = document.querySelector('#cancel_button')
+const cancelButton2 = document.querySelector('#cancel_button2')
+const createButtonMain = document.querySelector('#create3')
+const viewGraph = document.querySelector('#create4')
+const exitGraph = document.querySelector('#create5')
+const updateButton = document.querySelector('#update_button')
+createButtonMain.addEventListener('click', (e) => {
+    showCreate()
+})
+cancelButton.addEventListener('click', (e) => {
+    hideCreate()
+})
+cancelButton2.addEventListener('click', (e) => {
+    hideUpdate()
+})
+viewGraph.addEventListener('click', (e) => {
+    showGraph()
+})
+exitGraph.addEventListener('click', (e) => {
+    hideGraph()
+})
+function showGraph()  {
+    container6.style.display = ""
+    container2.style.display = "none"
+}
+function hideGraph()  {
+    container2.style.display = ""
+    container6.style.display = "none"
+}
 createButton.addEventListener('click', (e) => {
     e.preventDefault()
 
-    addDoc(colRef, { comment: crudForm.comment.value })
-        .then(() => {
-            crudForm.reset()
-            if (user_status == 1) {
-                location.href = location.href
-            }else{
-                location.replace(location.href + "?user_status=1")
-            }
+    let disYear = createForm.discontinuation_year.value
+    disYear = parseInt(disYear)
 
-        })
+    let relYear = createForm.release_year.value
+    relYear = parseInt(relYear)
+
+    let units = createForm.units_sold_in_millions.value
+    units = parseInt(units)
+
+    addDoc(colRef, {
+        ["Company"]: createForm.company.value,
+        ["Console Name"]: createForm.console_name.value,
+        ["Discontinuation year"]: disYear,
+        ["Released Year"]: relYear,
+        ["Type"]: createForm.type.value,
+        ["Units Sold in Millions"]: units
+    })
+    .then(() => {
+        createForm.reset()
+        hideCreate()
+        if (user_status == 1) {
+            location.href = location.href
+        }else{
+            location.replace(location.href + "?user_status=1")
+        }
+    })
 })
+function showCreate() {
+    container2.style.display = 'none'
+    container4.style.display = ''
+    container5.style.display = 'none'
+}
+function hideCreate() {
+    container4.style.display = 'none'
+    container2.style.display = ''
+    container5.style.display = 'none'
+}
+function showUpdate() {
+    container2.style.display = 'none'
+    container4.style.display = 'none'
+    container5.style.display = ''
+}
+function hideUpdate() {
+    container5.style.display = 'none'
+    container4.style.display = 'none'
+    container2.style.display = ''
+}
 
 function deleteTheDamnDoc(document) {
-    let docRef = doc(db, "comments", document)
+    let docRef = doc(db, "game-consoles", document)
     deleteDoc(docRef)
     .then(() => {
         if (user_status == 1) {
@@ -322,21 +421,60 @@ function deleteTheDamnDoc(document) {
 }
 
 var contForm = document.querySelector('.cont3');
-async function editTheDamnDoc(document, old_value) {
-    let docRef = doc(db, "comments", document)
-    contForm.updated_content.setAttribute('value', old_value)
-    contForm.updated_content.setAttribute('placeholder', 'Provide New Value')
-    hideDiv()
+async function editTheDamnDoc(
+    document, 
+    old_console_name, 
+    old_type,
+    old_company,
+    old_release_year,
+    old_discontinuation_year,
+    old_units
+) {
+    let docRef = doc(db, "game-consoles", document)
 
-    button3.addEventListener('click', (e) => {
+    updateForm.console_name.setAttribute('value', old_console_name)
+    updateForm.console_name.setAttribute('placeholder', 'Provide New Console Name')
+    
+    updateForm.type.setAttribute('value', old_type)
+    updateForm.type.setAttribute('placeholder', 'Provide New Type')
+
+    updateForm.company.setAttribute('value', old_company)
+    updateForm.company.setAttribute('placeholder', 'Provide New Company')
+
+    updateForm.release_year.setAttribute('value', old_release_year)
+    updateForm.release_year.setAttribute('placeholder', 'Provide New Year of Release')
+
+    updateForm.discontinuation_year.setAttribute('value', old_discontinuation_year)
+    updateForm.discontinuation_year.setAttribute('placeholder', 'Provide New Year of Discontinuation')
+
+    updateForm.units_sold_in_millions.setAttribute('value', old_units)
+    updateForm.units_sold_in_millions.setAttribute('placeholder', 'Provide New Units Sold in Millions')
+    
+    showUpdate()
+
+    updateButton.addEventListener('click', (e) => {
 
         e.preventDefault()
+
+        let disYear = updateForm.discontinuation_year.value
+        disYear = parseInt(disYear)
+    
+        let relYear = updateForm.release_year.value
+        relYear = parseInt(relYear)
+    
+        let units = updateForm.units_sold_in_millions.value
+        units = parseInt(units)
         
         updateDoc(docRef, { 
-            comment: contForm.updated_content.value 
+            ["Company"]: updateForm.company.value,
+            ["Console Name"]: updateForm.console_name.value,
+            ["Discontinuation year"]: disYear,
+            ["Released Year"]: relYear,
+            ["Type"]: updateForm.type.value,
+            ["Units Sold in Millions"]: units
         })
         .then(() => {
-            showDiv()
+            hideUpdate()
             if (user_status == 1) {
                 location.href = location.href
             }else{
@@ -352,7 +490,7 @@ async function editTheDamnDoc(document, old_value) {
 
 var button3 = document.getElementById('close_cont3')
 var button4 = document.getElementById('close_cont4')
-var container3 = document.querySelector('.container3')
+
 container3.style.display = 'none'
 button4.addEventListener('click', (e) => {
 
@@ -376,21 +514,42 @@ function showDiv() {
 const edit_icon = document.getElementById('read-icon1')
 const delete_icon = document.getElementById('read-icon2')
 function renderComment(doc){
-    let comment_container = document.createElement('div')
-    let comment_content = document.createElement('input')
+    let row = itemList.insertRow(-1)
+    row.setAttribute('class', 'row')
+    let c1 = row.insertCell(0)
+    let c2 = row.insertCell(1)
+    let c3 = row.insertCell(2)
+    let c4 = row.insertCell(3)
+    let c5 = row.insertCell(4)
+    let c6 = row.insertCell(5)
+    let c7 = row.insertCell(6)
+    c1.setAttribute('class', 'column')
+    c1.innerText = doc.data()["Console Name"]
+    c2.setAttribute('class', 'column')
+    c2.innerText = doc.data()["Type"]
+    c3.setAttribute('class', 'column')
+    c3.innerText = doc.data()["Company"]
+    c4.setAttribute('class', 'column')
+    c4.innerText = doc.data()["Released Year"]
+    c5.setAttribute('class', 'column')
+    c5.innerText = doc.data()["Discontinuation year"]
+    c6.setAttribute('class', 'column')
+    c6.innerText = doc.data()["Units Sold in Millions"]
+    c7.setAttribute('class', 'column')
     const edit_icon_copy = edit_icon.cloneNode(true)
     const delete_icon_copy = delete_icon.cloneNode(true)
-    comment_container.classList.add('item')
-    comment_content.setAttribute('type', 'text')
-    comment_content.setAttribute('id', 'item')
-    comment_content.setAttribute('value', doc.data().comment)
-    comment_content.setAttribute('data-id', doc.id)
-    comment_content.setAttribute('placeholder', doc.data().comment)
-    comment_content.disabled = true;
-    itemList.appendChild(comment_container)
-    comment_container.appendChild(comment_content)
-    comment_container.appendChild(edit_icon_copy)
-    comment_container.appendChild(delete_icon_copy)
+    c7.appendChild(edit_icon_copy)
+    c7.appendChild(delete_icon_copy)
+    // comment_content.setAttribute('type', 'text')
+    // comment_content.setAttribute('id', 'item')
+    // comment_content.setAttribute('value', doc.data().comment)
+    // comment_content.setAttribute('data-id', doc.id)
+    // comment_content.setAttribute('placeholder', doc.data().comment)
+    // comment_content.disabled = true;
+    // itemList.appendChild(row)
+    // comment_container.appendChild(comment_column)
+    // comment_container.appendChild(edit_icon_copy)
+    // comment_container.appendChild(delete_icon_copy)
 
     //deleting data
     delete_icon_copy.addEventListener('click', async (e) => {
@@ -407,12 +566,49 @@ function renderComment(doc){
     edit_icon_copy.addEventListener('click', async (e) => {
         e.preventDefault();
         try {
-            const old_value = await doc.data().comment
+            const old_console_name = await doc.data()["Console Name"]
+            const old_type = await doc.data()["Type"]
+            const old_company = await doc.data()["Company"]
+            const old_release_year = await doc.data()["Released Year"]
+            const old_discontinuation_year = await doc.data()["Discontinuation year"]
+            const old_units = await doc.data()["Units Sold in Millions"]
             const document = await doc.id;
-            editTheDamnDoc(document, old_value)
+            editTheDamnDoc(
+                document, 
+                old_console_name, 
+                old_type,
+                old_company,
+                old_release_year,
+                old_discontinuation_year,
+                old_units
+            )
         } catch (error) {
             console.log(error);
         }
     });
       
 }
+
+
+
+getDocs(colRef)
+    .then((snapshot) => {
+        let console_names = []
+        let console_units = []
+        snapshot.docs.forEach(doc => {
+            console_names.push(doc.data()["Console Name"])
+            console_units.push(doc.data()["Units Sold in Millions"])
+        })
+        var data = [
+            {
+              x: console_names,
+              y: console_units,
+              type: 'bar'
+            }
+          ];
+          
+        Plotly.newPlot('container6', data);
+    })
+
+
+
